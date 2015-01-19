68a0477a206e59ed7e5089f8ecffbb6c450858fb
Jacob Poul Richardt <spam@poulrichardt.dk>
2015-01-19 03:49:02 +0100

    Added boilerplate style

---
+++ TierList/TierList/App/Main.ts
@@ -0,0 +1,25 @@
+﻿declare module "Portal" { }
+declare module "text!../../Configuration.json" { var configuration: string; export = configuration; }
+declare var CacheBuster: number;
+
+requirejs.config({
+	paths: {
+		text:			"../Lib/requirejs/text",
+		jquery:			"../Lib/jquery/jquery.min",
+		knockout:		"../Lib/knockout/knockout.min",
+		bootstrap:		"../Lib/bootstrap/js/bootstrap.min",
+	},
+	shim: {
+		bootstrap: {
+			deps: ['jquery']
+		}
+	},
+	waitSeconds: 20,
+	urlArgs: "bust=" + CacheBuster
+});
+
+require(['NameConventionLoader', 'knockout', 'bootstrap'], (nameConventionLoader:any, knockout: any) =>
+{
+	knockout.components.loaders.push(new nameConventionLoader("TierList"));
+	knockout.applyBindings();
+});
---
+++ TierList/TierList/App/Shell/Shell.js
@@ -0,0 +1,8 @@
+define(["require", "exports"], function (require, exports) {
+    var Shell = (function () {
+        function Shell() {
+        }
+        return Shell;
+    })();
+    return Shell;
+});
